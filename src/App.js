8f8536bebb2afd67fc5ec515fa428cb6e4fa33ff
Rafael Assad <rafaassadeng@gmail.com>
2020-08-19 19:34:47 -0300

    campos iniciais e botão

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React from 'react';
-import logo from './trivia.png';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 import './App.css';
+import Home from './pages/home';
 
 export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          SUA VEZ
-        </p>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Home />
+    </BrowserRouter>
   );
 }
---
+++ src/pages/home/index.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import logo from '../../trivia.png';
+
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      disabled: true,
+      email: '',
+      player: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleChange(e) {
+    const { email, player } = this.state
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  handleClick() {
+    console.log('oi');
+  }
+
+  render() {
+    const { player, email } = this.state
+    const condition = ((player) && (email))? false:true;
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <main>
+          <label htmlFor="nome">Nome</label>
+          <input
+            name='player'
+            type="text"
+            id="nome"
+            data-testid="input-player-name"
+            onChange={this.handleChange}
+          />
+          <label htmlFor="email">E-mail</label>
+          <input
+            name='email'
+            type="text"
+            id="email"
+            data-testid="input-gravatar-email"
+            onChange={this.handleChange}
+          />
+          <button onClick={() => this.handleClick()} data-testid="btn-play" disabled={condition}>
+            Jogar
+          </button>
+        </main>
+      </div>
+    );
+  }
+}
+
+export default Home;
