f760eba17ca9858178be57d715f1df9777197d5a
Rafael Assad <rafaassadeng@gmail.com>
2020-08-21 08:04:35 -0300

    renderizando pagina do ranking

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Home from './pages/Home';
 import Settings from './pages/Settings';
 import Game from './pages/Game';
+import Ranking from './pages/Ranking';
 
 export default function App() {
   return (
@@ -12,10 +13,13 @@ export default function App() {
       <br />
       <Link to="/settings">Settings</Link>
       <br />
+      <Link to="/ranking">Ranking</Link>
+      <br />
       <Link to="/game">Game</Link>
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/settings" component={Settings} />
+        <Route path="/ranking" component={Ranking} />
         <Route path="/game" component={Game} />
       </Switch>
     </BrowserRouter>
--- src/pages/Ranking/index.js
+++ src/pages/Ranking/index.js
@@ -3,3 +3,17 @@ Os elementos com os nomes das pessoas que jogaram devem possuir o atributo data-
 Os elementos com as pontuações das pessoas que jogaram devem possuir o atributo data-testid com o valor player-score-${index}, onde ${index} é iniciado em zero
 O ranking deve ser armazenado no navegador através do localStorage.
 Leia a seção "Implementações técnicas" para mais detalhes */
+import React from 'react';
+import GameHeader from '../../components/GameHeader/index';
+
+const Ranking = () => (
+  <div>
+    <GameHeader />
+    <div data-testid="ranking-title">Ranking</div>
+    <button type="button" data-testid="btn-go-home">
+      Home
+    </button>
+  </div>
+);
+
+export default Ranking;
